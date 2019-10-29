89e9e5adf0195cb4976d93162fc6b27e0568ce5f
神樹桜乃 <kodama@sakuno.moe>
2019-10-30 03:07:43 +0800

    Add sponsors

---
+++ scripts/data/partners.js
@@ -0,0 +1,43 @@
+const partnersData = [
+  {
+    "order": 0,
+    "title": "Sponsor Organizations",
+    "id": "sponsor_org",
+    "items": [
+      {
+        "logoUrl": "../images/partners/paradigm.png",
+        "name": "Paradigm",
+        "order": 0,
+        "url": "https://www.paradigm.xyz/",
+        "id": "paradigm"
+      },
+      {
+        "logoUrl": "../images/partners/bxf_crypto.png",
+        "name": "BXF Crypto",
+        "order": 1,
+        "id": "bxf_crypto"
+      },
+      {
+        "logoUrl": "../images/partners/hashkey_captial.png",
+        "name": "Hashkey Capital",
+        "order": 2,
+        "url": "https://www.hashkey.com/",
+        "id": "hashkey_captial"
+      },
+      {
+        "logoUrl": "../images/partners/nervos.png",
+        "name": "Nervos",
+        "order": 3,
+        "url": "https://www.nervos.org/",
+        "id": "nervos"
+      },
+      {
+        "logoUrl": "../images/partners/celo.png",
+        "name": "Celo",
+        "order": 4,
+        "url": "https://celo.org/",
+        "id": "celo"
+      }
+    ]
+  }
+];
